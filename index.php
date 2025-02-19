bd374424fdf8f47393e600110be7ccf2e0bb09d3
utigernils <utigernils@gmail.com>
2025-02-19 06:49:16 +0100

    implemented a session handler and linked it into index.php

--- index.php
+++ index.php
@@ -2,11 +2,27 @@
 
 require_once 'src/config.php';
 require_once 'src/router.php';
+require_once 'src/session.php';
 require_once 'src/db.php';
 
-$config = new Config('config.ini');
-$router = new Router($config->get('content_type'),$config->get('base_path'));
-$db = new db($config->get('db_host'), $config->get('db_name'), $config->get('db_user'), $config->get('db_pass'));
+
+$config = new Config(configPath:
+    'config.ini'
+);
+
+$router = new Router(
+    contentType: $config->get('content_type'),
+    basePath:$config->get('base_path')
+);
+
+$session = new Session();
+
+$db = new db(
+    host:$config->get('db_host'), 
+    db:$config->get('db_name'),
+    user: $config->get('db_user'), 
+    pass:$config->get('db_pass')
+);
 
 function login() {
     return true;
