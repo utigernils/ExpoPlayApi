75ffa02b441735dab37354f8e59619e8e9589dfb
utigernils <utigernils@gmail.com>
2025-02-20 14:20:23 +0100

    implemented all player endpoints

--- controllers/playerController.php
+++ controllers/playerController.php
@@ -13,19 +13,79 @@ class playerController {
         $this->response = new Response();
     }
 
-    public function getPlayer() {
+    public function getPlayer($playerId) {
+        $player = $this->playerModell->get($playerId);
 
+        if ($player === false) {
+            $this->response->error(message:'Your request was blocked due to invalid credentials');
+        }
+        
+        if (!empty($player)) {
+            $this->response->setHeader(200);
+
+            $response = json_encode($player[0]);
+            echo $response;
+            exit();
+
+        } else {
+            $this->response->error(message:'Player not found', responseCode:404);
+        }
     }
 
-    public function updatePlayer() {
+    public function updatePlayer($playerId) {
+        $player = $this->playerModell->get($playerId);
 
+        if (empty($player)) {
+            $this->response->error(message:'Player not found', responseCode:404);
+        }
+
+        $jsonData = json_decode(file_get_contents('php://input'), true);
+
+        if (empty($jsonData)) {
+            $this->response->error(message:'No data provided', responseCode:400);
+        }
+
+        foreach ($jsonData as $field => $value) {
+            $player = $this->playerModell->set($playerId, $field, $value);
+            
+            if ($player === false) {
+                $this->response->error(message:'Your request was blocked due to invalid credentials');
+            }
+        }
+
+        $this->response->message(message:'Player updated successfully', responseCode:200);
     }
 
-    public function removePlayer() {
+    public function removePlayer($playerId) {
+        $player = $this->playerModell->get($playerId);
 
+        if (empty($player)) {
+            $this->response->error(message:'Player not found', responseCode:404);
+        }
+
+        $this->playerModell->delete($playerId);
+        $this->response->message(message:'Player deleted successfully', responseCode:200);
     }
 
     public function getAllPlayers() {
+        $orderBy = isset($_GET['orderBy']) ? $_GET['orderBy'] : null;
+        $direction = isset($_GET['desc']) ? $_GET['desc'] : null;
 
+        $player = $this->playerModell->get(orderBy: $orderBy, desc: $direction);
+
+        if ($player === false) {
+            $this->response->error(message:'Your request was blocked due to invalid credentials');
+        }
+        
+        if (!empty($player)) {
+            $this->response->setHeader(200);
+
+            $response = json_encode($player);
+            echo $response;
+            exit();
+
+        } else {
+            $this->response->error(message:'No Players found', responseCode:404);
+        }
     }
 }
