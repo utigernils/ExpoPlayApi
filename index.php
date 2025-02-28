6aa7ecc46f60be25c4250e20bb832ac7116de40d
utigernils <utigernils@gmail.com>
2025-02-28 09:04:25 +0100

    fix; pass all Preflight requests when in DEV mode

--- index.php
+++ index.php
@@ -27,7 +27,8 @@ $config = new Config(configPath:
 );
 
 if ($config->get('mode') === 'development') {
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: http://localhost:4200');
+    header('Access-Control-Allow-Credentials: true');
     header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
     header('Access-Control-Allow-Headers: Content-Type, Authorization');
 }
@@ -63,6 +64,13 @@ $quizController = new quizController($session, $quizModell);
 $userController = new userController($session, $dashboardUserModell);
 
 $loginController = new loginController($session, $db);
+
+if ($config->get('mode') === 'development') {
+    if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+        http_response_code(200);
+        exit();
+    }
+}
 
 $router->addRoute(
     method: 'GET', 
