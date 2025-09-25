faf0ecdab832f9815825d19f189e313984d272f8
Nils Utiger <nils.utiger@suva.ch>
2025-09-25 11:21:26 +0200

    feat; setup endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,20 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/test', function () {
-    return response()->json(['message' => 'echo']);
-});
+use App\Http\Controllers\{
+    PlayerController,
+    ExpoController,
+    QuizController,
+    ConsoleController,
+    PlayedQuizController,
+    QuestionController
+};
+
+Route::apiResources([
+    'players' => PlayerController::class,
+    'expos' => ExpoController::class,
+    'quizzes' => QuizController::class,
+    'consoles' => ConsoleController::class,
+    'played-quizzes' => PlayedQuizController::class,
+    'questions' => QuestionController::class,
+]);
